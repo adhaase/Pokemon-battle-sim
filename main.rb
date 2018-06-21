4f8055899588810cdeab21679d0a3d56c9f31213
Alex Haase <adhaase@umich.edu>
2018-06-21 17:47:28 -0400

    updated main

--- main.rb
+++ main.rb
@@ -8,7 +8,9 @@ require_relative 'pokemon_battle.rb'
 pikachu = Pokemon.new("Pikachu", "Electric", 24, 31)
 bulbasaur = Pokemon.new("Bulbasaur", "Grass", 43, 19)
 charmander = Pokemon.new("Charmander", "Fire", 32, 51)
+squirtle = Pokemon.new("Squirtle", "Water", 56, 22)
 
 pikachu.pokemon_info
 bulbasaur.pokemon_info
 charmander.pokemon_info
+squirtle.pokemon_info
