b6fab06bea4392f70b4e38ed92e58d59903f9293
Alex Haase <adhaase@umich.edu>
2018-06-23 03:18:35 -0400

    player class updates

--- player.rb
+++ player.rb
@@ -8,17 +8,37 @@ class Player
 	def initialize(name, party) # ctor
 		# instance variables 
 		@name = name
-		@party = Array.new(6) # max of 6 Pokemon can be in one party
+		@party = Array.new(6) # max of 6 Pokemon can be in one party (contains Pokemon objects)
 	end
 
 	def print_party(party)
-		party.each {|x| puts x}
+		party.each do |pkmn|
+			puts pkmn
+		end
 	end
 
 	def player_info # prints player name and party
+		puts "Player name: #{@name}"
+		print_party(@party)
 	end
 end
 
+pikachu = Pokemon.new("Pikachu", "Electric", 24, 31)
+bulbasaur = Pokemon.new("Bulbasaur", "Grass", 43, 19)
+charmander = Pokemon.new("Charmander", "Fire", 32, 51)
+squirtle = Pokemon.new("Squirtle", "Water", 56, 22)
 
-Alex = Player.new 
-Alex.party
+def print_array(array)
+	puts "Print array function"
+	array.each do |i|
+		puts i
+	end
+end
+
+alex_party = [pikachu, bulbasaur]
+print_array(alex_party)
+
+
+Alex = Player.new("Alex", alex_party)
+#Alex.player_info
+#Alex.print_party(alex_party)
