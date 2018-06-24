7035145f8481cf7bf82a8e0922a2b856556982f9
Alex Haase <adhaase@umich.edu>
2018-06-24 03:04:13 -0400

    updated Player class and pkmn party info

--- player.rb
+++ player.rb
@@ -8,37 +8,33 @@ class Player
 	def initialize(name, party) # ctor
 		# instance variables 
 		@name = name
-		@party = Array.new(6) # max of 6 Pokemon can be in one party (contains Pokemon objects)
+		@party = party # max of 6 Pokemon can be in one party (contains Pokemon objects)
 	end
 
-	def print_party(party)
-		party.each do |pkmn|
-			puts pkmn
+	def print_party # a player knows the Pokemon party, no need to pass argument
+		@party.each do |pkmn|
+			puts "Name: #{pkmn.name} | Type: #{pkmn.type} | HP: #{pkmn.health_points} | ATK: #{pkmn.attack_power}"
 		end
 	end
 
 	def player_info # prints player name and party
 		puts "Player name: #{@name}"
-		print_party(@party)
+		#print "Party size: "
+		#puts @party.length
+		print_party
 	end
 end
+
+
+
+# TEST
 
 pikachu = Pokemon.new("Pikachu", "Electric", 24, 31)
 bulbasaur = Pokemon.new("Bulbasaur", "Grass", 43, 19)
 charmander = Pokemon.new("Charmander", "Fire", 32, 51)
 squirtle = Pokemon.new("Squirtle", "Water", 56, 22)
 
-def print_array(array)
-	puts "Print array function"
-	array.each do |i|
-		puts i
-	end
-end
+alex_party = [pikachu, squirtle, charmander, bulbasaur]
+alex = Player.new("Alex", alex_party)
 
-alex_party = [pikachu, bulbasaur]
-print_array(alex_party)
-
-
-Alex = Player.new("Alex", alex_party)
-#Alex.player_info
-#Alex.print_party(alex_party)
+alex.player_info
